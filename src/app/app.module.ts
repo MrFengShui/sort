f13c39047fa26a2404eb8b00b744bf5c45e07337
MrFengShui <luansongjian@gmail.com>
2025-04-16 14:52:53 +0800

    调试主题样式

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [ AppComponent ],
+    imports: [
+        BrowserModule,
+        ButtonModule,
+        AppRoutingModule
+    ],
+    providers: [
+        provideAnimationsAsync()
+    ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
